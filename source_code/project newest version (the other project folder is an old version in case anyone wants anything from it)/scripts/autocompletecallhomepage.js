4da9270405170f88a668d7317bb2a5127303b712
ramoGraph <131772024+ramoGraph@users.noreply.github.com>
2023-04-28 00:06:08 +0200

    ---

--- source_code/project newest version (the other project folder is an old version in case anyone wants anything from it)/scripts/autocompletecallhomepage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $("#from").autocomplete({
-        source: "autocompletehomepage.php"
-    });
-    $("#to").autocomplete({
-        source: "autocompletehomepage.php"
-    });
-});
