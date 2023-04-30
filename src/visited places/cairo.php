606671f0929392d2a9e06754853f7f8d924148fd
ramoGraph <131772024+ramoGraph@users.noreply.github.com>
2023-04-30 18:32:12 +0300

    Added extra pages (most visited places)

---
+++ src/visited places/cairo.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="description" content="Learn about Cairo and Riyadh airports, including information on airport facilities, airlines, and destinations.">
+	<title>Cairo</title>
+	<link rel="stylesheet" href="../../styles/visitedplaces.css">
+	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+</head>
+<body>
+	<header>
+		<div class="navigation-bar">
+			<nav>
+			<ul>
+				<li><h1 style="margin: 0;"><a href="../homepage.php">Gamasa Airlines</a></h1><h5 style="margin: 0; text-align: end;">Flying is Easier With Us</h5></li>
+			</ul>
+			</nav>
+		</div>
+		<div class="back-image" style='background-image:url("../../images/recommended locations/cairo-egypt.jpg");'>
+		</div>
+	</header>
+    <main>
+		<div class="title">
+			<h1 style = "font-size: xxx-large">Cairo</h1>
+			<article>
+			<div class = "container">
+				<img src="../../images/recommended locations/cairo-egypt.jpg" alt="Cairo Airport" style = "width:100%">
+				&emsp;
+				The Egyptian capital, Cairo, is abundant with a huge number of various hotels, which focus luxury hotels in three main locations: 
+				The first is in the eastern part of the city near Cairo International Airport for travelers who want to spend one or two nights in Cairo, 
+				and the second site is the city center and the area overlooking the magical Nile River، There you will find the finest hotels in the city for lovers of residence near many important tourist attractions such as the Egyptian Museum, Cairo Tower and Salahuddin Castle, and not far from Khan Al-Khalili and the famous shopping areas in the capital. 
+				The third region where there are some distinctive hotels also in Cairo is the area surrounding the pyramids of the Sphinx, which present their guests with stunning views on the most important monument in Egypt and the world at large. 
+				In all three regions, as well as in the Heliopolis region, Nasr City, and Zamalek, there are many hotels that bear the names of the most famous international chains, such as:Sheraton, Hilton, Sofitel, Marriott, Meridian, Four Seasons, Grand Hayat and recently joined the list called "Ritz Carlton"، 
+				It provides the best services and facilities such as luxury curses and restaurants, as well as its accessories from shopping centers and beauty centers, and many and many high-quality services such as those found all over the world. 
+				Travelers with a budget of limited budget can easily find cheap hotels throughout Cairo, especially in the city center.
+			</div>
+			</article>
+            <div class = "container">
+			<aside>
+				<h2>Cairo Airport</h2>  
+				<p>&emsp;Located in Heliopolis, Cairo International Airport is the busiest airport in Egypt and serves as a hub for several airlines, including EgyptAir and Nile Air. The airport has three terminals and offers a variety of services and amenities, including duty-free shopping, restaurants, and lounges.</p>
+				<table>
+					<thead>
+						<tr>
+							<th>Terminals</th>
+							<th>Airlines</th>
+							<th>Destinations</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Terminal 1</td>
+							<td>EgyptAir, Nile Air</td>
+							<td>Domestic and international</td>
+						</tr>
+						<tr>
+							<td>Terminal 2</td>
+							<td>Multiple airlines</td>
+							<td>International</td>
+						</tr>
+						<tr>
+							<td>Terminal 3</td>
+							<td>Multiple airlines</td>
+							<td>International</td>
+						</tr>
+					</tbody>
+				</table>
+			</aside>
+            </div>
+		</div>
+    </main>
+</body>
+</html>
---
+++ src/visited places/riyadh.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="description" content="Learn about Cairo and Riyadh airports, including information on airport facilities, airlines, and destinations.">
+	<title>Riyadh</title>
+	<link rel="stylesheet" href="../../styles/visitedplaces.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+	<header>
+		<div class="navigation-bar">
+			<nav>
+			<ul>
+				<li><h1 style="margin: 0;"><a href="../homepage.php">Gamasa Airlines</a></h1><h5 style="margin: 0; text-align: end;">Flying is Easier With Us</h5></li>
+			</ul>
+			</nav>
+		</div>
+	</header>
+	<div class="back-image" style='background-image:url("../../images/recommended locations/riyadh-saudi.jpg")'></div>
+    <main>
+		
+		<div class="title">
+			<h1 style = "font-size: xxx-large">Riyadh</h1>
+			<article>
+			<div class = "container">
+				<img src="../../images/recommended locations/riyadh-saudi.jpg" alt="Riyadh Airport" style = "width:100%">
+				
+				
+                &emsp;The Saudi capital, "Riyadh City", is characterized by the presence of many picturesque hotels, which are the largest and most famous cities in Saudi Arabia. 
+                    The various Riyadh hotels are concentrated in 3 basic locations: 
+                    The first is North Riyadh and Alia and there you will find many Riyadh hotels near the most famous tourist areas such as the Fasla Tower, which is designed in a hierarchical manner and ends with an elegant glass ball، 
+                    It also includes commercial markets and various business offices, in addition to a large number of luxury hotels.
+                    The second site is the city center, which is considered one of the most vital areas in Riyadh and includes many and many important tourist attractions, for example the Kingdom Tower, which is one of the largest commercial shopping centers in Riyadh, 
+                    the National Museum that highlights the bright history of the Kingdom of Saudi Arabia, the fortress and museum of your historical designer, which is part of the ancient town of Riyadh، 
+                    And other archaeological and tourist places that many tourists from all over the world come to visit, the third site is south of Riyadh، Where you can easily reach the hotels of the city of Riyadh through King Khalid International Airport, which is about 20 km from the center of Riyadh، 
+                    There is also a zoo, which is one of the most entertaining places in Riyadh for adults and children.                    
+                    In all three regions, and the rest of Riyadh, there are many hotels that bear the names of the most famous international chains,
+                     such as: Sheraton, Hilton, Sofitel, Marriott, Meridian, Four Seasons, and Grand Hayat، It provides the best services and facilities such as luxury curses and restaurants, as well as its accessories from shopping centers and beauty centers, and many and many high-quality services such as those found all over the world. For low-budget travelers, they can easily find cheap hotels throughout Riyadh, especially in the city center.
+				
+				</div>
+			</article>
+            <div class = "container">
+			<aside>
+				<h2>Riyadh Airport</h2>  
+                <p>&emsp;King Khalid International Airport is the main airport serving Riyadh, the capital of Saudi Arabia. The airport is located 35 kilometers north of the city and has four terminals. The airport offers a range of services and amenities, including duty-free shopping, restaurants, and lounges.</p>
+				<table>
+					<thead>
+						<tr>
+							<th>Terminals</th>
+							<th>Airlines</th>
+							<th>Destinations</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Terminal 1</td>
+							<td>EgyptAir, Nile Air</td>
+							<td>Domestic and international</td>
+						</tr>
+						<tr>
+							<td>Terminal 2</td>
+							<td>Multiple airlines</td>
+							<td>International</td>
+						</tr>
+						<tr>
+							<td>Terminal 3</td>
+							<td>Multiple airlines</td>
+							<td>International</td>
+						</tr>
+					</tbody>
+				</table>
+			</aside>
+            </div>
+		</div>
+    </main>
+</body>
+</html>
