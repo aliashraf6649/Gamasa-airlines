7b929c3b78cfc997552b9c92d3fd388fc2e25d68
ramoGraph <131772024+ramoGraph@users.noreply.github.com>
2023-04-30 19:05:03 +0300

    removed todo comment

--- src/logout.php
+++ src/logout.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 session_unset();
-//TODO: CLEAR ALL SET COOKIES
 session_destroy();
 setcookie("from-to", $from_to_encode, time()-1000, "/");
 setcookie("ticket", $ticket_encode,time()-1000,"/"); 
